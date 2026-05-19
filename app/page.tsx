ecf1e25891899748cd03148053b0eda4b01c01c7
darthk0der <andre.s.llewellyn@gmail.com>
2026-05-19 16:21:51 -0400

    fix: force dynamic rendering to skip db query at build time

--- app/page.tsx
+++ app/page.tsx
@@ -7,6 +7,7 @@ import { AdminFooter } from '@/components/AdminFooter';
 import { StickyNav } from '../components/StickyNav';
 
 export const maxDuration = 300;
+export const dynamic = 'force-dynamic';
 
 export default async function Home() {
   
